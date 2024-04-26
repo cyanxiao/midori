62cea0235ff8329da4667d985e1455096c277e44
Chell <im.chell@outlook.com>
2024-04-26 21:11:26 +0200

    Add file transfer functionality to Green Orchestrator

--- green_orchestrator/core.py
+++ green_orchestrator/core.py
@@ -1,89 +1,54 @@
 import paramiko
 import time
 from typing import List
+from .file_transfer import PodFileTransfer
 
 
 class Orchestrator:
     def __init__(self, hostname: str, username: str, password: str, subject_path: str, trial_interval: int,
-                 trial_derivatives: List[str], trial_end_waiting_time: int,
-                 pod_name_start: str, file_path_in_pod: str, local_file_path: str):
-        self.hostname = hostname
-        self.username = username
-        self.password = password
-        self.subject_path = subject_path
-        self.trial_interval = trial_interval
-        self.trial_derivatives = trial_derivatives
-        self.trial_end_waiting_time = trial_end_waiting_time
-        self.pod_name_start = pod_name_start
-        self.file_path_in_pod = file_path_in_pod
-        self.local_file_path = local_file_path
-        self.ssh = paramiko.SSHClient()
-        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.connect()
+                 pod_name_start: str, file_path_in_pod: str, node_save_file_path: str, local_save_file_path: str, namespace='default'):
+        self.__subject_path = subject_path
+        self.__file_path_in_pod = file_path_in_pod
+        self.__local_save_file_path = local_save_file_path
+        self.__pod_name_start = pod_name_start
+        self.__namespace = namespace
+        self.__node_save_file_path = node_save_file_path
 
-    # launch an SSH connection to the remote server
-    def connect(self):
+        self.__ssh = paramiko.SSHClient()
+        self.__ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
-            self.ssh.connect(hostname=self.hostname,
-                             username=self.username, password=self.password)
+            self.__ssh.connect(hostname=hostname,
+                               username=username, password=password)
+            print("Connected to the remote server.")
         except Exception as e:
             print(f"Failed to connect to the remote server: {e}")
 
-    # navigate to the specified directory
-    def navigate(self):
-        stdin, stdout, stderr = self.ssh.exec_command(
-            f"cd {self.subject_path}")
-        print(stdout.read().decode())
+    # run the orchestrator
+    def run(self):
+        command = "cd " + self.__subject_path + " && skaffold delete && skaffold run"
+        self.__execute(command=command)
+        self.__pause(float(100))
+        self.__pft = PodFileTransfer(
+            self.__ssh, self.__pod_name_start, self.__namespace)
+        self.__pft.transfer_file_from_pod(
+            self.__file_path_in_pod, self.__node_save_file_path)
+        self.__close()
+
+    """
+    Basic Functions
+    """
 
     # pause for the specified interval
-    def pause(self):
-        time.sleep(self.trial_interval)
+    def __pause(self, interval: float):
+        print(f"Pausing for {interval} seconds...")
+        time.sleep(interval)
 
     # execute the specified command
-    def execute(self, command: str):
-        stdin, stdout, stderr = self.ssh.exec_command(command)
+    def __execute(self, command: str):
+        stdin, stdout, stderr = self.__ssh.exec_command(command)
+        print(command)
         print(stdout.read().decode())
-        time.sleep(self.trial_end_waiting_time)
-
-    # collect data from the remote server
-    def transfer_file_from_pod(self):
-        try:
-            # Finding the pod
-            stdin, stdout, stderr = self.ssh.exec_command(
-                f"kubectl get pods --no-headers -o custom-columns=':metadata.name'")
-            pod_list = stdout.read().decode('utf-8').split()
-            target_pod = next(
-                (pod for pod in pod_list if pod.startswith(self.pod_name_start)), None)
-
-            if not target_pod:
-                print(f"No pod starts with {self.pod_name_start}")
-                return
-
-            print(f"Target pod found: {target_pod}")
-
-            # Copy from the pod to the node
-            node_temp_path = "/tmp/" + target_pod + "_file"
-            copy_cmd = f"kubectl cp default/{target_pod}:{
-                self.file_path_in_pod} {node_temp_path}"
-            stdin, stdout, stderr = self.ssh.exec_command(copy_cmd)
-            errors = stderr.read().decode()
-            if errors:
-                print(f"Error copying file from pod: {errors}")
-                return
-            print("File copied to node successfully.")
-
-            # Transfer file from node to local machine using SFTP
-            sftp = self.ssh.open_sftp()
-            sftp.get(node_temp_path, self.local_file_path)
-            print("File transferred to local machine successfully.")
-
-            # Cleanup node
-            self.execute(f"rm {node_temp_path}")
-            print("Temporary files on node removed.")
-
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
 
     # close the SSH connection
-    def close(self):
-        self.ssh.close()
+    def __close(self):
+        self.__ssh.close()
---
+++ green_orchestrator/file_transfer.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+
+class PodFileTransfer:
+    def __init__(self, ssh, pod_name_start, namespace='default'):
+        self.ssh = ssh
+        self.pod_name_start = pod_name_start
+        self.namespace = namespace
+
+    def transfer_file_from_pod(self, file_path_in_pod: str, node_saving_path: str) -> None:
+        try:
+            target_pod = self.find_pod()
+            if target_pod is None:
+                print(f"No pod starts with {self.pod_name_start}")
+                return
+
+            node_temp_path = self.copy_file_to_node(
+                target_pod, file_path_in_pod, node_saving_path)
+            if node_temp_path is None:
+                return
+
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+    def find_pod(self) -> Optional[str]:
+        command = "kubectl get pods --no-headers -o custom-columns=':metadata.name'"
+        stdin, stdout, stderr = self.ssh.exec_command(command)
+        pod_list = stdout.read().decode('utf-8').split()
+        return next((pod for pod in pod_list if pod.startswith(self.pod_name_start)), None)
+
+    def copy_file_to_node(self, pod: str, file_path_in_pod: str, node_saving_path: str) -> Optional[str]:
+        copy_cmd = f"kubectl cp {
+            self.namespace}/{pod}:{file_path_in_pod} {node_saving_path}"
+        print(copy_cmd)
+        stdin, stdout, stderr = self.ssh.exec_command(copy_cmd)
+        errors = stderr.read().decode()
+        if errors:
+            print(f"Error copying file from pod: {errors}")
+            return None
+        print("File copied to node successfully.")
+        return node_saving_path
