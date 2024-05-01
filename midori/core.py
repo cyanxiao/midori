f3a9ff51a6b23a72a13e96072ee612968468ba37
Chell <im.chell@outlook.com>
2024-05-01 13:04:36 +0200

    Refactor Orchestrator class in core.py to include prometheus_pod_name_start parameter and improve file transfer functionality

--- midori/core.py
+++ midori/core.py
@@ -8,7 +8,7 @@ from .treatments_helper import get_treatments
 class Orchestrator:
     def __init__(self, hostname: str, username: str, password: str, trial_interval: int, trial_timespan: int,
                  pod_name_start: str, file_path_in_pod: str, node_save_file_path: str, local_save_file_path: str, variables:  Dict[str, List[str]],
-                 namespace: str = 'default', subject_path: str = '.') -> None:
+                 namespace: str = 'default', subject_path: str = '.', prometheus_pod_name_start: str = 'prometheus-server') -> None:
         self.__subject_path: str = subject_path
         self.__file_path_in_pod: str = file_path_in_pod
         self.__local_save_file_path: str = local_save_file_path
@@ -17,6 +17,7 @@ class Orchestrator:
         self.__pod_name_start: str = pod_name_start
         self.__namespace: str = namespace
         self.__base_node_save_file_path: str = node_save_file_path
+        self.__prometheus_pod_name_start: str = prometheus_pod_name_start
         self.__ssh: paramiko.SSHClient = paramiko.SSHClient()
         self.__ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
@@ -39,7 +40,17 @@ class Orchestrator:
             command: str = "cd " + self.__subject_path + \
                 " && skaffold delete && skaffold run"
             self.__execute(command=command)
+
             self.__pause(interval=self.__trial_timespan)
+
+            # # Collect energy data
+            self.__energy_collector: PodFileTransfer = PodFileTransfer(
+                self.__ssh, self.__prometheus_pod_name_start, self.__namespace)
+            energy_node_save_path = f"{
+                self.__base_node_save_file_path}/{treatment}/energy.txt"
+            self.__energy_collector.execute_query_in_pod(
+                node_saving_path=energy_node_save_path)
+
             # Append the treatment name to the base node save path
             treatment_node_save_path = f"{
                 self.__base_node_save_file_path}/{treatment}"
@@ -47,6 +58,9 @@ class Orchestrator:
                 self.__ssh, self.__pod_name_start, self.__namespace)
             self.__pft.transfer_file_from_pod(
                 self.__file_path_in_pod, treatment_node_save_path)
+        # skaffold_delete_command: str = "cd " + \
+        #     self.__subject_path + " && skaffold delete"
+        # self.__execute(command=skaffold_delete_command)
         self.__close()
 
     def checkout_branch_based_on_treatment(self, treatment: str) -> None:
--- midori/file_transfer.py
+++ midori/file_transfer.py
@@ -1,4 +1,5 @@
 from typing import Optional
+import time
 
 
 class PodFileTransfer:
@@ -49,3 +50,56 @@ class PodFileTransfer:
             return None
         print("Files copied to node successfully.")
         return node_saving_path
+
+    def wait_for_pod_to_start(self, timeout: int):
+        start_time = time.time()
+        print("Waiting for the pod to be in 'Running' state...")
+        while time.time() - start_time < timeout:
+            command = f"kubectl get pod $(kubectl get pods --no-headers -o custom-columns=':metadata.name' | grep '{
+                self.pod_name_start}') -o jsonpath='{{.status.phase}}'"
+            stdin, stdout, stderr = self.ssh.exec_command(command)
+            status = stdout.read().decode().strip()
+
+            if status == 'Running':
+                print("Pod is in 'Running' state.")
+                return True
+            time.sleep(1)  # Wait for 5 seconds before checking again.
+
+        print(f"Timed out waiting for the pod to be in 'Running' state after {
+              timeout} seconds.")
+        return False
+
+    def execute_query_in_pod(self, node_saving_path: str, start_time: Optional[int] = None, end_time: Optional[int] = None) -> None:
+        # Default time span: from current time - 60 seconds to current time
+        if end_time is None:
+            end_time = int(time.time())
+        if start_time is None:
+            start_time = end_time - 60
+
+        pod = self.find_pod()
+        if not pod:
+            print("No matching pod found.")
+            return
+
+        # Ensure directory and file exist
+        mkdir_cmd = f"mkdir -p {node_saving_path.rsplit('/', 1)[0]}"
+        touch_cmd = f"touch {node_saving_path}"
+        self.ssh.exec_command(mkdir_cmd)
+        self.ssh.exec_command(touch_cmd)
+        print(f"Directory and file ensured at {node_saving_path}")
+
+        # Format the query to cover the specified time span
+        query_cmd = f"kubectl exec {
+            pod} -- wget -qO- 'http://localhost:9090/api/v1/query_range?query=scaph_host_power_microwatts%20%2F%201000000&start={start_time}&end={end_time}&step=1'"
+        print(f"Executing query from {start_time} to {end_time}")
+        stdin, stdout, stderr = self.ssh.exec_command(query_cmd)
+        response = stdout.read().decode()
+
+        # Append output to the file on the remote server
+        append_cmd = f"echo \"{response}\" >> {node_saving_path}"
+        _, _, stderr = self.ssh.exec_command(append_cmd)
+        errors = stderr.read().decode().strip()
+        if errors:
+            print(f"Failed to append the response to file: {errors}")
+
+        print("Query successfully executed and output appended to the file.")
