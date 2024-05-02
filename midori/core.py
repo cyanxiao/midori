906ac832de04d03fd7c6588df111d0c376d6350e
Chell <im.chell@outlook.com>
2024-05-02 17:40:09 +0200

    Refactor Orchestrator class in core.py to remove unused variables and improve code readability

--- midori/core.py
+++ midori/core.py
@@ -21,25 +21,15 @@ class Orchestrator:
         trial_timespan: int,
         end_trial_plugins: List[Type[PluginHelper]],
         after_trial_cooling_time: int,
-        pod_name_start: str,
-        file_path_in_pod: str,
-        node_save_file_path: str,
         variables: Dict[str, List[str]],
-        namespace: str = "default",
-        subject_path: str = ".",
-        prometheus_pod_name_start: str = "prometheus-server",
+        subject_path: str,
     ) -> None:
         self.__subject_path: str = subject_path
-        self.__file_path_in_pod: str = file_path_in_pod
         self.__before_trial_cooling_time: int = before_trial_cooling_time
         self.__trial_timespan: int = trial_timespan
         self.__setup_plugins: List[Type[PluginHelper]] = setup_plugins
         self.__after_trial_cooling_time: int = after_trial_cooling_time
         self.__end_trial_plugins: List[Type[PluginHelper]] = end_trial_plugins
-        self.__pod_name_start: str = pod_name_start
-        self.__namespace: str = namespace
-        self.__base_node_save_file_path: str = node_save_file_path
-        self.__prometheus_pod_name_start: str = prometheus_pod_name_start
         self.__ssh: paramiko.SSHClient = paramiko.SSHClient()
         self.__ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
@@ -91,27 +81,6 @@ class Orchestrator:
                     previous_output=output,
                 )
                 output = plugin.execute()
-
-            # # Collect energy data
-            # self.__energy_collector: PodHelper = PodHelper(
-            #     self.__ssh, self.__prometheus_pod_name_start, self.__namespace
-            # )
-            # energy_node_save_path = f"{
-            #     self.__base_node_save_file_path}/{treatment}/energy.json"
-            # self.__energy_collector.execute_query_in_pod(
-            #     node_saving_path=energy_node_save_path,
-            #     query_cmd=self.__energy_collector.construct_query_cmd(),
-            # )
-
-            # # Append the treatment name to the base node save path
-            # treatment_node_save_path = f"{
-            #     self.__base_node_save_file_path}/{treatment}"
-            # self.__pft: PodHelper = PodHelper(
-            #     self.__ssh, self.__pod_name_start, self.__namespace
-            # )
-            # self.__pft.transfer_file_from_pod(
-            #     self.__file_path_in_pod, treatment_node_save_path
-            # )
 
             # After Trial Cooling time
             pause(interval=self.__after_trial_cooling_time)
