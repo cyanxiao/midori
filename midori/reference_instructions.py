e2cc5b2571063d20dd410a62af8c289b4ab53735
Chell <im.chell@outlook.com>
2024-05-02 17:52:29 +0200

    Remove unused reference instructions in reference_instructions.py

--- midori/reference_instructions.py
+++
@@ -1,5 +0,0 @@
-start_skaffold = "skaffold start"
-end_skaffold = "skaffold delete"
-list_pods = "kubectl get pods --all-namespaces"
-list_files_inside_pod = "kubectl exec -it loadgenerator-xxx -c main ls"
-copy_file_from_pod = "kubectl cp default/loadgenerator-xxx:/loadgen/result_stats.csv ~/research/result_stats.csv"
